565477b81fd77228ce7134516d1dd1159dc39dd8
KushRabadia <58183575+KushRabadia@users.noreply.github.com>
2021-03-01 05:01:36 -0500

    Update and rename List Linked(C) to List Linked.c

--- Data-Structures/List Linked.c
+++ Data-Structures/List Linked.c
@@ -1,11 +1,3 @@
-/*
- * ---------------------------------
- * Student Name: Kush Rabadia Kantilal Nanji
- * Student ID: 1932311270
- * Student Email: nanj1170@mylaurier.ca
- * ---------------------------------
- */
-
 # include <stdio.h>
 # include <stdlib.h>
 # include <assert.h>
